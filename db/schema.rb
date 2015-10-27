6a7fbf549395713f64f27beb5732d76e16b25a69
Philip DesJean <pdesjean@elevenfiftyconsulting.com>
2015-10-27 17:32:13 -0400

    Yeah Yeah Yeah

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027173726) do
+ActiveRecord::Schema.define(version: 20151027212603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,11 @@ ActiveRecord::Schema.define(version: 20151027173726) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "post_type",  default: 0
+    t.integer  "post_type",   default: 0
+    t.integer  "category_id"
   end
 
+  add_index "posts", ["category_id"], name: "index_posts_on_category_id", using: :btree
+
+  add_foreign_key "posts", "categories"
 end
